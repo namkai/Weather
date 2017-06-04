c19f287912b28139aaba2cf5039bdccd169d1eb8
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-03 19:26:47 -0700

    changed back to http

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
 
 export const API_KEY = 'appid=4a32b866b9fe2b80a174d8e905040a87';
-export const CURRENT_WEATHER = `https://api.openweathermap.org/data/2.5/weather?${API_KEY}`;
-export const FORECAST_WEATHER = `https://api.openweathermap.org/data/2.5/forecast?${API_KEY}`
+export const CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?${API_KEY}`;
+export const FORECAST_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?${API_KEY}`
