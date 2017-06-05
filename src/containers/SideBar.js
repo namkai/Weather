106c824384fd135263813e9487d255b0f6a1d569
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-06-05 15:26:32 -0700

    fixed bug that hid searchbar on mobile device

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -6,24 +6,16 @@ import Cities from '../components/Cities';
 import SearchBar from '../components/SearchBar';
 import citySelector from '../selectors/selected_stored_cities';
 
-export const SideBar = ({ storedCities, fetchWeather, updateQuery, input, updateStoredCities, clearStoredCities }) => (
+export const SideBar = ({ storedCities, fetchWeather, updateStoredCities, clearStoredCities }) => (
   <div className="col-md-3 sidebar">
     <nav className="sidebar-nav">
       <div className="sidebar-header">
-        <button
-          className="nav-toggler nav-toggler-md sidebar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#nav-toggleable-md"
-        ><span className="sr-only">Toggle nav</span>
-        </button>
         <a className="sidebar-brand img-responsive" href="#">
           <span style={{ color: '#1ec8ff' }} className="icon icon-drop sidebar-brand-icon" />
         </a>
       </div>
-
+	    <SearchBar fetchWeather={fetchWeather} />
       <div className="collapse nav-toggleable-md" id="nav-toggleable-md">
-        <SearchBar fetchWeather={fetchWeather} />
         <Cities
           storedCities={storedCities}
           fetchWeather={fetchWeather}
@@ -35,18 +27,22 @@ export const SideBar = ({ storedCities, fetchWeather, updateQuery, input, update
       </div>
     </nav>
   </div>
-	);
+);
 
 SideBar.defaultProps = {
 	storedCities: PropTypes.arrayOf(PropTypes.string, PropTypes.object),
 	fetchWeather: PropTypes.func,
+	updateStoredCities: PropTypes.func,
+	clearStoredCities: PropTypes.func,
 };
 
 SideBar.propTypes = {
 	storedCities: PropTypes.arrayOf(PropTypes.string, PropTypes.object),
 	fetchWeather: PropTypes.func,
+	updateStoredCities: PropTypes.func,
+	clearStoredCities: PropTypes.func,
 };
 
-const mapStateToProps = state => ({ storedCities: citySelector(state)});
+const mapStateToProps = state => ({ storedCities: citySelector(state) });
 
 export default connect(mapStateToProps, actions)(SideBar);
