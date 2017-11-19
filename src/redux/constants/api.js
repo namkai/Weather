1b9fc270cd26831601c20fd8cb61781b709d051d
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-11-19 11:59:10 +0100

    Delete api.js

--- src/redux/constants/api.js
+++
@@ -1,4 +0,0 @@
-
-export const API_KEY = 'appid=4a32b866b9fe2b80a174d8e905040a87';
-export const CURRENT_WEATHER = `http://api.openweathermap.org/data/2.5/weather?${API_KEY}`;
-export const FORECAST_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?${API_KEY}`
